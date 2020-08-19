ef89b18278b27b6df410a8209e469d346cb161ac
Grant Joseph <grantj150@yahoo.com>
2020-08-18 21:57:45 -0400

    include X11 if on Linux. Only include EGL if WARLOCK_OPENGL_ES is defined

--- warlock.h
+++ warlock.h
@@ -1,6 +1,12 @@
 #ifndef WARLOCK_H
 #define WARLOCK_H
 
+#ifdef __linux__
+    #include <X11/Xlib.h>
+    #include <X11/Xutil.h>
+    #include <X11/Xatom.h>
+#endif // __linux__
+
 #ifdef WARLOCK_OPENGL_ES
     #include <EGL/egl.h>
 #endif // WARLOCK_OPENGL_ES
